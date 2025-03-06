2ec9fcbfdbbbbf1eb2b4b4e518e55c71ca1b725e
Blocky <pablo.garciahernande@edu.uah.es>
2025-03-06 16:47:06 +0100

    Add files via upload

---
+++ Sesion2/cadena.s
@@ -0,0 +1,104 @@
+.data
+    tam = 20+1
+    buffer: .space tam
+    buflower: .space tam
+    bufupper: .space tam
+    bufswap: .space tam
+    msg: .ascii "\nEscribe una cadena de menos de 20 caracteres:\n"
+    len = . - msg
+
+.text
+.global _start
+_start:
+    /* Solicitar al usuario que escriba una cadena */
+    movl  $4,     %eax
+    movl  $1,     %ebx
+    movl  $msg,   %ecx
+    movl  $len,   %edx
+    int   $0x80
+
+    /* Leer la cadena desde el teclado */
+    movl $3,      %eax
+    movl $0,      %ebx
+    movl $buffer, %ecx
+    movl $tam,    %edx
+    int  $0x80
+
+contar:
+    /* Contar el numero de caracteres leidos */
+    movb buffer(%esi), %al
+    cmpb $0, %al
+    je siguiente
+    inc %edi    /* La cuenta se guarda en %edi */
+    inc %esi    
+    jmp contar
+
+siguiente:
+    /* Restamos 1 a %edi para no tomar en cuenta el final de la cadena */
+    subl $1,   %edi
+
+    /* Poner en minuscula todo*/
+    movl %edi,  %ecx
+    movl $0,    %esi
+
+lower:
+    movb  buffer(%esi), %al
+    orb   $0b00100000,  %al
+    movb  %al, buflower(%esi)
+    incl  %esi
+    loop lower
+
+    addl  $1,  %esi
+    movb  $10, buflower(%esi)
+
+    movl $4,        %eax
+    movl $1,        %ebx
+    movl $buflower, %ecx
+    movl $tam,      %edx
+    int  $0x80
+
+    /* Poner en mayuscula todo*/
+    movl %edi, %ecx
+    movl $0, %esi
+
+upper:
+    movb buffer(%esi), %al
+    andb $0b1011111,   %al
+    movb %al, bufupper(%esi)
+    incl %esi
+    loop upper
+
+    addl  $1,   %esi 
+    movb  $10,  bufupper(%esi)
+
+    movl $4,        %eax
+    movl $1,        %ebx
+    movl $bufupper, %ecx
+    movl $tam,      %edx
+    int $0x80
+
+    /* Intercambiar mayusculas y minusculas entre si */
+    movl %edi,  %ecx
+    movl $0,    %esi
+
+swap:
+    movb buffer(%esi), %al
+    xorb $0b00100000,  %al
+    movb %al, bufswap(%esi)
+    incl %esi
+    loop swap
+
+    addl  $1,   %esi 
+    movb  $10,  bufswap(%esi)
+
+    movl $4,        %eax
+    movl $1,        %ebx
+    movl $bufswap,  %ecx
+    movl $tam,      %edx
+    int $0x80
+
+    /* Finalizamos el programa */
+    movl  $1,   %eax
+    movl  $0,   %ebx
+    int   $0x80
+    
